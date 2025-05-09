b751771b75dbcae854d310d4bdc65d7f18d8d6fb
Mostafa-N-E <84570342+Mostafa-N-E@users.noreply.github.com>
2025-05-09 14:27:21 +0330

    .

---
+++ tavasoul/tapsell.dart
@@ -0,0 +1,37 @@
+class AdsConstant {
+  static const tapsellAppId = 'oajpmboordqfkcfdocdnoappsgdbljatbghqkjknkcmbgnfqispjbknjgnchbjpfmdpjqk';
+  static const adiveryAppId = "842fdec0-3e39-49f1-b3da-25c4bd68b3c1";
+  // static const appName = 'ملکوت';
+
+  static const Tapsell = {
+    "appId": 'oajpmboordqfkcfdocdnoappsgdbljatbghqkjknkcmbgnfqispjbknjgnchbjpfmdpjqk',
+
+    "REWARDED": '67edc1087eb9371a591b6475',
+    "INTERSTITIAL_BANNER": "67edc0b4891cc40554c414f6",
+    "NATIVE_BANNER": "67edc0887eb9371a591b6474",
+    "INTERSTITIAL_VIDEO": "",
+    "NATIVE_VIDEO": "",
+    "STANDARD_BANNER": "",
+  };
+  static const Adivery = {
+    "appId": '55602750-4a50-4c6f-9eda-ad0aef96d63b',
+
+    "REWARDED": 'e00995fc-fb45-45c0-a7a4-d1b5f6192c65',
+    "INTERSTITIAL": '04cbcec6-88c7-4571-9f90-e4b3abdf0bef',
+    "NATIVE_BANNER":"0793b48e-7e90-45a5-8ef1-5909caa7c78f",
+    "INTERSTITIAL_VIDEO": "",
+    "NATIVE_VIDEO": "",
+    "STANDARD_BANNER": "",
+  };
+  static const AdMob = {
+    "appId": '',
+
+    "REWARDED": '',
+    "INTERSTITIAL": '',
+    "NATIVE_BANNER":"",
+    "INTERSTITIAL_VIDEO": "",
+    "NATIVE_VIDEO": "",
+    "STANDARD_BANNER": "",
+  };
+
+}
